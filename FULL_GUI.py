87e53fb81996fbae02906664ae3badc8d28813bd
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2020-01-14 20:25:12 +0200

    del FULL_G

--- FULL_GUI.py
+++
@@ -1,9 +0,0 @@
-from tinydb import TinyDB, Query
-from spellchecker import SpellChecker
-from random import random
-from tkinter import *
-import time
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-
